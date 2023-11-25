9f86bdfbab7ce2eb9145f9a2015e93f8b10db901
abdoulyass <perruce4356@gmail.com>
2023-11-25 16:27:29 +0100

    Initial commit

---
+++ client.php
@@ -0,0 +1,20 @@
+<?php include('tete.php')?>
+
+
+	
+	<!-- SIDEBAR -->
+	<section id="sidebar">
+		<?php include('dash.php') ?>
+	
+	<section id="content">
+		<?php include('navbar.php')?>
+       <main class="d-flex">
+          <?php include('pp-v.php')?>
+		  <div class="pic" style="width:300px ">
+			 <img src="pic.png" >
+           </div>
+		   </main>
+	  <?php include('footer.php')?>
+     	<script src="script.js"></script>
+</body>
+</html>
